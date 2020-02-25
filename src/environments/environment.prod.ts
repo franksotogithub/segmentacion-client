5f761a4578d40c0aef351262dbbbae9ccfd77d30
franksotogithub <franksoto2012@gmail.com>
2020-02-25 14:59:42 -0500

    actualizando menu

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiEndPointData: 'https://segmentacion-project.herokuapp.com/',
-  apiEndPointMap: 'http://arcgis1.inei.gob.pe:6080/arcgis/rest/services/DESARROLLO/DATACRIM/MapServer/',
+  apiEndPointMap: 'http://arcgis4.inei.gob.pe:6080/arcgis/rest/services/DESARROLLO/SEGMENTACION_TESIS/MapServer',
   apiEndPointPdf: 'http://proyectos.inei.gob.pe/cpv2017/croquis-listado/urbano/CPV2017/',
 };
